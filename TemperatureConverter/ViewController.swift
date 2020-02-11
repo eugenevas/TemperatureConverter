f47e3e1b63d68b4a5f2292df6d4aa6b85a95efc0
Evgeniy Vasilyev <eugene.vas25@gmail.com>
2020-02-11 23:13:38 +0300

    added new pics

--- TemperatureConverter/ViewController.swift
+++ TemperatureConverter/ViewController.swift
@@ -10,10 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        override var preferredStatusBarStyle: UIStatusBarStyle {
+            return .lightContent
+        }
+    
 
 
 }
